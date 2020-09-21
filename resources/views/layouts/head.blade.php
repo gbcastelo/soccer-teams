8180564720b3d6e3213389f16f60065f3517d4f8
Gabriel Castelo <gabriel.castelo@weclever.co>
2020-09-21 01:53:55 -0300

    Adding favicon

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,6 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="shortcut icon" href="{{asset('favicon.ico')}}"/>
 <title>Soccer Teams - Divida suas equipes facilmente!</title>
 {{-- Project CSS --}}
 <link rel="stylesheet" href="{{asset('css/styles.css')}}">
